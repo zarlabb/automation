5048fc8489a73475575320a00019ae476ae1a992
zarlabb <bodya.gerebukh@gmail.com>
2019-11-11 16:51:38 +0200

    first and second tasks

--- first project/src/first/Rar.java
+++ first project/src/first/Rar.java
@@ -1,10 +1,29 @@
 package first;
 
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Rar {
 
-	public static void main(String[] args) {
-		String a = "abcsaaghbbbcddefff";
-		System.out.println(a);
-	}
 
+	public static void main(String[] args) {
+        String text = "abcsaaghbbbcddefff";
+
+
+        Matcher m = Pattern.compile("(.)\\1+").matcher(text);
+        while (m.find()) {
+            String sub = m.group();
+            int len = sub.length();
+ 
+            String sub1 = sub.substring(0,1);
+            String sub2 = text.replaceAll(m.group(), len + sub1);  
+            System.out.println(sub2);
+          
+          
+          
+        }
+     
+    }
 }
+
